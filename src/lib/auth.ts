939d6fbcd64f76de71ca522a95fac0e6a32f920c
Keaan Mirchandani <keaanm@outlook.com>
2024-11-17 04:14:52 -0800

    commit three

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,4 +6,10 @@ import { db } from "./db/db";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
   adapter: DrizzleAdapter(db),
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
 });
